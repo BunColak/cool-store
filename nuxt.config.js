42c35814aaec9211b26a56bc5ad9ad7364c43087
Bun Colak <bun.colak@motius.de>
2020-12-29 00:58:45 +0100

    snipcart stuff

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,4 +2,11 @@ export default {
   head: {
     title: "My Nuxt.js Application!",
   },
+  modules: [
+    '@nuxtjs/snipcart',
+    '@nuxtjs/strapi'
+  ],
+  snipcart: {
+    key: process.env.SNIPCART_API_KEY
+  }
 };
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
 <template>
     <div>
-        Hello there test
+        <div v-for="tshirt in tshirts" :key="tshirt.id">
+            <h4>{{tshirt.name}}</h4>
+            <h5>hello</h5>
+            <button class="snipcart-add-item" v-bind="$snipcart.bindProduct(tshirt)">Add to cart</button>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'IndexPage',
+    async asyncData({ params, $strapi }) {
+      const tshirts = await $strapi.find('tshirts')
+      console.log(tshirts[0].images);
+      return {tshirts}
+    }
+}
+</script>
